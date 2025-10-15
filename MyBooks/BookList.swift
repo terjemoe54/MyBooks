25c9a9db676deddb302598053f8a00311d60d8e5
Terje Moe <terjemoe54@hotmail.com>
2025-10-15 09:45:28 +0200

    Del4_MedFilterSort

---
+++ MyBooks/BookList.swift
@@ -0,0 +1,18 @@
+//
+//  BookList.swift
+//  MyBooks
+//
+//  Created by Terje Moe on 15/10/2025.
+//
+
+import SwiftUI
+
+struct BookList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BookList()
+}
